604f1204e967fb967a4d962f0dbf6b37a017da9a
anasofich <castellanos_chopas@hotmail.com>
2021-10-07 21:45:02 -0500

    added color selector

--- script.js
+++ script.js
@@ -8,19 +8,37 @@ async function start() {
 }
 
 function init() {
-  console.log("init");
+  console.log("init()");
   const clockCase = document.querySelector("#case");
   const buckle = document.querySelector("#buckle");
   const keeper = document.querySelector("#keeper");
-  console.log(clockCase, buckle, keeper);
+  //console.log(clockCase, buckle, keeper);
 
   setColor(clockCase, "red");
   setColor(buckle, "red");
   setColor(keeper, "blue");
+
+  clockCase.addEventListener("click", (event) => {
+    setColor(event.target, currentColor);
+  });
+
+  buckle.addEventListener("click", (event) => {
+    setColor(event.target, currentColor);
+  });
+
+  keeper.addEventListener("click", (event) => {
+    setColor(event.target, currentColor);
+  });
+
+  document.querySelectorAll(".colorSelector").forEach((element) => {
+    element.addEventListener("click", (event) => {
+      currentColor = event.target.style.backgroundColor;
+    });
+  });
 }
 
 function setColor(element, colorString) {
   console.log(element, colorString);
-  //document.querySelectorAll(".colorSelector").forEach((element) => {});
+  document.querySelectorAll(".colorSelector").forEach((element) => {});
   element.style.fill = colorString;
 }
