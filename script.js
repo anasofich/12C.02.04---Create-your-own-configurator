8f7af5359302b038efcddd33fa7ffc75e5e2cbdb
anasofich <castellanos_chopas@hotmail.com>
2021-10-07 21:27:11 -0500

    adding svg ans starting color selector

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 document.addEventListener("DOMContentLoaded", start);
 
 async function start() {
-  let response = await fetch("SVG/white-watch.svg");
+  let response = await fetch("white-watch2.svg");
   let mySvgData = await response.text();
   document.querySelector("section").innerHTML = mySvgData;
   init();
 }
+
 function init() {
   console.log("init");
+  const clockCase = document.querySelector("#case");
+  const buckle = document.querySelector("#buckle");
+  const keeper = document.querySelector("#keeper");
+  console.log(clockCase, buckle, keeper);
+
+  setColor(clockCase, "red");
+  setColor(buckle, "red");
+  setColor(keeper, "blue");
+}
+
+function setColor(element, colorString) {
+  console.log(element, colorString);
+  //document.querySelectorAll(".colorSelector").forEach((element) => {});
+  element.style.fill = colorString;
 }
